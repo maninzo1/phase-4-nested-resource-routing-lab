de9e141bada1d934e31a4f3c4895d4b8c953676c
MaryAninzo <aninzom211@gmail.com>
2021-11-07 14:06:33 -0500

    commit tree fd6f436b4cb4d12fdff1a4e2fb42da1ba32a35bd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,45 @@
 class ItemsController < ApplicationController
 
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  rescue ActiveRecord::RecordNotFound
+    render_user_not_found
+    
   end
 
+  def show
+    
+      item = Item.find(params[:id])
+      render json: item
+    rescue ActiveRecord::RecordNotFound
+      render_user_not_found
+    
+  end
+
+  def create
+    
+    user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      render json: item, status: :created
+
+  end
+
+  private
+
+  def render_user_not_found
+    render json: { error: "User is not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  
 end
